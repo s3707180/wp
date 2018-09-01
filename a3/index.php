45e20e8bb0da612978ee1749a9cdc80890616019
als <s37071810@rmit.edu.au>
2018-09-01 15:53:38 +1000

    fixes to the assignment

--- a3/index.php
+++ a3/index.php
@@ -34,8 +34,7 @@
 
     <main>
       <article id='Website Under Construction'>
-    <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
-        <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
+        <img width=600 src='../../media/maintenance.png' alt='Website Under Construction' />
       </article>
     </main>
 
